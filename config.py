02e9042e2a9f41925ae60b7e8bc7113018496ea4
Darshan <92586012+XDacchuX@users.noreply.github.com>
2022-03-21 21:24:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,9 +12,9 @@ if not ENV:
   api_id = ""
   api_hash = ""
   ARQ_API_BASE_URL = "https://thearq.tech"
-  BOT_USERNAME = "feritapibot"
+  BOT_USERNAME = "Kittu_Robot"
   KONTOL = "Feri"
-  MEMEK = "https://telegra.ph/file/e10d3feead2c64950b98f.mp4"
+  MEMEK = "https://telegra.ph/file/4ebf97569f757accc4b36.jpg"
 else:
   bot_token = str(environ.get("BOT_TOKEN", None))
   ARQ_API_KEY = str(environ.get("ARQ_API_KEY", None))
@@ -22,6 +22,6 @@ else:
   api_id = int(environ.get("API_ID", 6))
   api_hash = str(environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e"))
   ARQ_API_BASE_URL = "https://grambuilders.tech"
-  BOT_USERNAME = str(environ.get("BOT_USERNAME", "feritapibot"))
+  BOT_USERNAME = str(environ.get("BOT_USERNAME", "Kittu_Robot"))
   KONTOL = str(environ.get("BOT_NAME", "Feri"))
-  MEMEK = str(environ.get("MEDIA", "https://telegra.ph/file/e10d3feead2c64950b98f.mp4"))
+  MEMEK = str(environ.get("MEDIA", "https://telegra.ph/file/4ebf97569f757accc4b36.jpg"))
